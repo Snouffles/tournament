3ec4885b9b587eafc7a7c7e55e90de0c8572a5bb
Snouffles <donovan.payan@gmail.com>
2022-04-24 11:17:58 +0200

    cleaning of the code

--- scripts.js
+++ scripts.js
@@ -4,9 +4,17 @@ let players = [
     {id: 2, seed: 3, name: "Elsa", goal:{for: 0, against:0, difference:0}, pts: 0, draw: 0, loose: 0, victory: 0, rankingPts: 0},
     {id: 3, seed: 4, name: "Linus", goal:{for: 0, against:0, difference:0}, pts: 0, draw: 0, loose: 0, victory: 0, rankingPts: 0},
     {id: 4, seed: 5, name: "Robert", goal:{for: 0, against:0, difference:0}, pts: 0, draw: 0, loose: 0, victory: 0, rankingPts: 0} ];
-
-    let matches = [];
-
+    
+    let matches = [{}];
+    
+    for(i = 0; i < players.length; i++){
+        for(y = 0; y < players.length; y++){
+            if(i != y){
+                matchID = i.toString()+y.toString();
+                matches.push({id: matchID, score: {team1: "", team2: ""}})
+            }
+        }
+    }
 
 
 Table();
@@ -39,98 +47,34 @@ document.querySelectorAll(".square").forEach(square =>{
 
     square.addEventListener("click", function(){
       
-        for(let i = 0; i < players.length; i++){
-            for(let y = 0; y < players.length; y++){
-               let  newMatchToPlay = `match${players[i].id}${players[y].id}`;
-                
-                if(this.dataset.id === newMatchToPlay){
-
-                    document.querySelector(".modal").innerHTML = 
-                  
-                       ` <div class="matchModal">
-                            <div>
-                                <p>${players[i].name}
-                                <input id="score${players[i].id}" type="number" value="0" min = "0"/>
-                            </div>
-                            <div>
-                                vs
-                            </div>
-                            <div>
-                                <p>${players[y].name}
-                                <input id="score${players[y].id}" type="number" value="0" min = "0"/>
-                            </div>
-                            <button>ok</button>
-                        </div>
-                    `
-                    ;
-
-                    document.querySelector(".matchModal button").addEventListener("click", e => {
-                    
-                            
-                       
-                        let iResult = document.getElementById(`score${players[i].id}`).value;
+        if(square.innerText != ""){
+            for(let i = 0; i < players.length; i++){
+                for(let y = 0; y < players.length; y++){
+                    let  newMatchToPlay = `match${players[i].id}${players[y].id}`;
+                    if(this.dataset.id === newMatchToPlay){
+                        showModalChange();
+                        modalChange(i, y);
                         
-                        let yResult = document.getElementById(`score${players[y].id}`).value;
                         
-
-                        if(iResult === yResult){
-
-                            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>Draw <br>${iResult} - ${yResult}<p>`;
-                            players[i].draw  += 1;
-                            players[i].pts = players[i].victory * 3 + players[i].draw;
-                            players[i].goal.for =+ iResult;
-                            players[i].goal.against =+ yResult;
                        
-                            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>Draw <br>${yResult} - ${iResult}</p>`;
-                            players[y].draw += 1;
-                            players[y].pts = players[y].victory * 3 + players[y].draw;
-                            players[y].goal.for =+ yResult;
-                            players[y].goal.against =+ iResult;
-                            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
-                            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
-                          
-
-                        }else if(iResult > yResult){
-
-                            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>${players[i].name} <br>${iResult} - ${yResult}<p>`;
-                            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>${players[i].name} <br>${yResult} - ${iResult}</p>`;
-                            players[i].victory += 1;
-                            players[y].defeat += 1;
-                            players[i].pts =  players[i].victory * 3 + players[i].draw;
-                            players[i].goal.for =+ iResult;
-                            players[i].goal.against =+ yResult;
-                            players[y].goal.for =+ yResult;
-                            players[y].goal.against =+ iResult;
-
-                            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
-                            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
-                            
-                        }else{
-
-                            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>${players[y].name} <br>${iResult} - ${yResult}<p>`;
-                            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>${players[y].name} <br>${yResult} - ${iResult}</p>`;
-                            players[y].victory +=1;
-                            players[i].defeat +=1;
-                            console.log("victory" + players[i].victory);
-                            players[y].pts = players[y].victory * 3 + players[y].draw; 
-                            players[y].goal.for =+ yResult;
-                            players[y].goal.against =+ iResult;
-                            players[i].goal.for =+ iResult;
-                            players[i].goal.against =+ yResult;
-                            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
-                            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
-                            
-                          
-                        }
-                        Table();     
-                        document.querySelector(".modal").innerHTML = "";
-                    })
-                
+                    }
                 }
             }
-        }
+        }else {
+        
+            for(let i = 0; i < players.length; i++){
+                for(let y = 0; y < players.length; y++){
+                    let  newMatchToPlay = `match${players[i].id}${players[y].id}`;
+                    if(this.dataset.id === newMatchToPlay){
+
+                        showModal(i,y)
+                        matchScore(i,y);
+                           
+                    }
+                }
+            }  
+        }   
     })
-    
 })
 
 
@@ -165,15 +109,153 @@ function sortTable (table){
     });
 }
 
+function showModal(i, y){
+    document.querySelector(".modal").innerHTML = 
+                  
+    ` <div class="matchModal">
+         <div>
+             <p>${players[i].name}
+             <input id="score${players[i].id}" type="number" value="0" min = "0"/>
+         </div>
+         <div>
+             vs
+         </div>
+         <div>
+             <p>${players[y].name}
+             <input id="score${players[y].id}" type="number" value="0" min = "0"/>
+         </div>
+         <button>ok</button>
+     </div>
+ `
+ ;
+}
 
+function showModalChange(i, y){
+    document.querySelector(".modal").innerHTML = 
+                  
+    ` <div class="matchModal">
+         <div>
+             <p> This games has already been played. Do you wish to change the result </p>
+         <button id="showModalChangeYes">Yes</button><button id="showModalChangeNo">No</button>
+     </div>
+`
+
+}
+
+function modalChange(i, y){
+    document.getElementById("showModalChangeYes").addEventListener("click", e=>{
+        removeMatchScore(i,y);
+        showModal(i, y);
+    })
+    document.getElementById("showModalChangeNo").addEventListener("click", e=>{
+        closeModal();
+    })
+   
+}
+
+function closeModal(){
+    document.querySelector(".modal").innerHTML = "";
+}
+
+
+function matchScore(i,y){
+    console.log("f " + i + " " + y);
+
+    document.querySelector(".matchModal button").addEventListener("click", e => {
+                    
+                        
+                       
+        let iResult = document.getElementById(`score${players[i].id}`).value;
+        
+        let yResult = document.getElementById(`score${players[y].id}`).value;
+        
+        console.log(i.toString()+y.toString())
+
+        for(x=0; x < matches.length; x++){
+            if(matches[x].id === i.toString()+y.toString()){
+                matches[x].score.team1 = iResult;
+                matches[x].score.team2 = yResult;
+                
+            }
+            if(matches[x].id === y.toString()+i.toString()){
+                matches[x].score.team1 = iResult;
+                matches[x].score.team2 = yResult;
+        
+            }
+        }
+
+        if(iResult === yResult){
+
+            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>Draw <br>${iResult} - ${yResult}<p>`;
+            players[i].draw  += 1;
+            players[i].pts = players[i].victory * 3 + players[i].draw;
+            players[i].goal.for =+ iResult;
+            players[i].goal.against =+ yResult;
+         
+       
+            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>Draw <br>${yResult} - ${iResult}</p>`;
+            players[y].draw += 1;
+            players[y].pts = players[y].victory * 3 + players[y].draw;
+            players[y].goal.for =+ yResult;
+            players[y].goal.against =+ iResult;
+            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
+            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
+          
+
+        }else if(iResult > yResult){
+
+            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>${players[i].name} <br>${iResult} - ${yResult}<p>`;
+            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>${players[i].name} <br>${yResult} - ${iResult}</p>`;
+            players[i].victory += 1;
+            players[y].defeat += 1;
+            players[i].pts =  players[i].victory * 3 + players[i].draw;
+            players[i].goal.for =+ iResult;
+            players[i].goal.against =+ yResult;
+            players[y].goal.for =+ yResult;
+            players[y].goal.against =+ iResult;
+
+            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
+            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
+            
+        }else{
+
+            document.querySelector(`.${players[i].name}${players[i].id}vs${players[y].id}${players[y].name}`).innerHTML = `<p>${players[y].name} <br>${iResult} - ${yResult}<p>`;
+            document.querySelector(`.${players[y].name}${players[y].id}vs${players[i].id}${players[i].name}`).innerHTML = `<p>${players[y].name} <br>${yResult} - ${iResult}</p>`;
+            players[y].victory +=1;
+            players[i].defeat +=1;
+            console.log("victory" + players[i].victory);
+            players[y].pts = players[y].victory * 3 + players[y].draw; 
+            players[y].goal.for =+ yResult;
+            players[y].goal.against =+ iResult;
+            players[i].goal.for =+ iResult;
+            players[i].goal.against =+ yResult;
+            players[i].goal.difference = players[i].goal.for - players[i].goal.against;
+            players[y].goal.difference = players[y].goal.for - players[y].goal.against;
+            
+          
+        }
+        Table();
+        closeModal();
+    })
+}
+    
+function removeMatchScore(i,y){
+    for(x = 1; x < matches.length; x++){
+        let goalx, goaly;
+
+        if(matches[x].id === i.toString() + y.toString()){
+            goalx = matches[x].score.team1;
+            goaly = matches[x].score.team2;
+            console.log(goalx +" "+ goaly)
+        }
+    }
+}
 
 function Table(){
 
   
     
     sortTable(players);
-
-    
 
     document.querySelector(".ranking").innerHTML = ` 
     
@@ -191,8 +273,8 @@ function Table(){
 
     for(let i = 0; i < players.length; i++){
         let difference = players[i].goal.for - players[i].goal.against;
-        let pts = (players[i].victory * 3) + players[i].draw;
-        console.log(players[i].victory );
+        // let pts = (players[i].victory * 3) + players[i].draw;
+       
         
             
             document.querySelector(".ranking").innerHTML+= 
@@ -209,27 +291,4 @@ function Table(){
     }
   
 
-}
-
-
-
-for(i=0; i < players.length; i++){
-    for(y=0; y < players.length;y++){
-        let player1 = players[i].id;
-        let player2 = players[y].id;
-        let matchId = `${player1}vs${player2}`
-        let matchReturnId = `${player2}vs${player1}`
-        if(player1 != player2 && (!(matches.includes(matchId)) && !(matchId.includes(matchReturnId)))){
-            matches.push(matchId);
-            matches.push(matchReturnId);
-    }
-    console.log("test: " + players[i].id);
-    }
-}
-
-
-
-
-for(i=0; i < matches.length; i++){
-    console.log("match " + matches[i]);
 }
